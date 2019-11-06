e9481babf765503f8fc20565fb7f16d16deae670
creative90 <wezeibekwe675@gmail.com>
2019-11-06 14:49:00 +0100

    Adding a new files

---
+++ src/app/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import {Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+
+  private_albumUrl = '../assets/album.json';
+  _albumUrl;
+  constructor(private _http: Http) { }
+
+
+  getAlbum(id: number) {
+
+  return this._http.get(this._albumUrl).map((reponse) => reponse.json());
+  
+  }
+  
+
+}
